196240f47c64353fa27286fabb2068dd8324965b
Tazeek <tazeek.rakib@gmail.com>
2017-08-08 12:36:53 +0800

    Coding semantic tags extraction function done

--- comment_scraper.py
+++ comment_scraper.py
@@ -1,12 +1,9 @@
 from utils import extractPage
 
 from bs4 import BeautifulSoup
-from os import listdir
 
-import urllib.request
-import requests
 import re
-import time
+import pickle
 
 def getComments(soup_comments):
 
@@ -38,25 +35,23 @@ def getComments(soup_comments):
 		comment = row_data.text
 
 		# Some comments are edited and the datastamp of edited message is extracted
-		comment = re.sub(r"Message edited by author .*", '', comment)
+		#comment = re.sub(r"Message edited by author .*", '', comment)
 
 		# Some comments have urls
-		comment = re.sub(r'^https?:\/\/.*[\r\n]*', '', comment, flags=re.MULTILINE)
+		#comment = re.sub(r'^https?:\/\/.*[\r\n]*', '', comment, flags=re.MULTILINE)
 		
 		# Append the cleaned comment
 		image_comments.append(comment)
 
 	print(image_comments)
+	exit()
 
 	return image_comments
 
 def getMetadata(soup_meta):
 
 	# Meta data is to be stored in a json format
-	semantic_json = {}
-
-	# Extract using BeautifulSoup
-	soup_meta = extractPage(url)
+	semantic_list = []
 
 	# Extract Semantic Tags
 	semantic_tags = soup_meta.findAll(
@@ -65,35 +60,52 @@ def getMetadata(soup_meta):
 		tag.attrs['href'].startswith('/photo_gallery.php')
 	)
 
+	# Loop tag by tag, if any
 	for tag in semantic_tags:
 
-		semantic_name = tag.text
-		semantic_id = int(re.findall('\d+', tag['href'])[0])
+		semantic_id = re.findall('\d+', tag['href'])[0]
 
-		semantic_json[semantic_id] = semantic_name
+		semantic_list.append(semantic_id)
 
-	return semantic_json
+	# If no tags found, add 0
+	if len(semantic_list) == 0:
+		semantic_list.append('0')
+
+	return semantic_list
 
 def scraping():
 
 	# AVA Image URL
 	AVA_URL_FOR_ID = 'http://www.dpchallenge.com/image.php?IMAGE_ID={}'
 
-	exit()
+	# Load Pickle file
+	with open('first_half.txt','rb') as fh:
+		image_id_list = pickle.load(fh)
 
-	for image_id in range(AVA_LAST_ID+1, latest_image_id+1):
+	for image_id in image_id_list:
 
 		# Get the image page URL
-		#url = AVA_URL_FOR_ID.format(image_id)
+		url = AVA_URL_FOR_ID.format(image_id)
 
 		# Extract Page
-		#page_extract = extractPage(url)
+		page_extract = extractPage(url)
+
+		# Get image metadata
+		image_meta = [image_id] + getMetadata(page_extract)
+
+		# Store image meta in string: 'ID, Tag_IDs'
+		image_meta = ' '.join(image_meta) + '\n'
 
 		# Get image comments
 		#image_comments = getComments(page_extract)
 
-		# Get image metadata
-		#image_meta = getMetadata(page_extract)
+		# Clean image comments
+
+		# Append to 'AVA 2.0 Semantics'
+		#with open('AVA 2.0 Image Semantics.txt', 'a') as append_file:
+		#	append_file.write(image_meta)
+
+		exit()
 
 	return
 
