85881f8700e6b9237efb33d5a429b18239927ff6
Tazeek <tazeek.rakib@gmail.com>
2017-08-10 17:18:43 +0800

    Merge script created

---
+++ merge.py
@@ -0,0 +1,17 @@
+# HINT: Use Image ID as foreign key
+
+# Load 'AVA 2.0 Image Semantics.txt'
+
+# Split lines after Index 0 (Index 0 is id, rest are semantic tag ids)
+
+# Find the maximum length after it
+
+# Append 0s wherever necessary
+
+# Create new string
+
+# Open 'AVA 2.0'
+
+# Semantic Tag IDs goes before challenge ID (Index -1)
+
+# Store again (Follow AVA 1.0 format)
